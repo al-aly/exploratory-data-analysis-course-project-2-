5b6f7c135a27fd118860079de2d39d6cf6bc90d1
al-aly <aly0701@gmail.com>
2016-07-17 13:21:37 +0800

    Add files via upload

---
+++ plot5.r
@@ -0,0 +1,38 @@
+## get working dirctory
+getwd()
+## set working directory
+setwd('~/Desktop/cousera/Exploratory Data Analysis/week4/proj2')
+## download data file
+data = "exdata_data.zip"
+if(!file.exists(data)){
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip",
+                destfile =data, method = "curl")
+}
+## Step 1:read downloaded file (rdsformat)
+NEI <- readRDS("exdata_data/summarySCC_PM25.rds")
+SCC <- readRDS("exdata_data/Source_Classification_Code.rds")
+
+# merge the two data sets 
+if(!exists("NEISCC")){
+  NEISCC <- merge(NEI, SCC, by="SCC")
+}
+library(ggplot2)
+# How have emissions from motor vehicle sources changed from 1999-2008 in Baltimore City?
+
+# 24510 is Baltimore, see plot2.R
+# Searching for ON-ROAD type in NEI
+# Don't actually know it this is the intention, but searching for 'motor' in SCC only gave a subset (non-cars)
+subsetNEI <- NEI[NEI$fips=="24510" & NEI$type=="ON-ROAD",  ]
+
+aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetNEI, sum)
+
+
+
+png("plot5.png", width=840, height=480)
+g <- ggplot(aggregatedTotalByYear, aes(factor(year), Emissions))
+g <- g + geom_bar(stat="identity") +
+  xlab("year") +
+  ylab(expression('Total PM'[2.5]*" Emissions")) +
+  ggtitle('Total Emissions from motor vehicle (type = ON-ROAD) in Baltimore City, Maryland (fips = "24510") from 1999 to 2008')
+print(g)
+dev.off()
